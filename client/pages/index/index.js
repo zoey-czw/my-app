6c84846888b0e874c1c86969f4fbd2d69649000a
chenggan <nhzxr2025@163.com>
2020-11-17 21:50:49 +0800

    home 第一次提交

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'H',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
